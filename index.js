8e234417111fa6c5a29b28d88d5d7a45dda017e7
xzy <xzy@xzydeMacBook-Pro.local>
2020-09-23 17:10:21 +0800

    2

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ module.exports = exports = function(webot){
     },
     handler: function(info){
       var reply = {
-        title: '感谢你收听webot机器人',
+        title: '感谢你关注Q比特',
         pic: 'https://raw.github.com/node-webot/webot-example/master/qrcode.jpg',
         url: 'https://github.com/node-webot/webot-example',
         description: [
@@ -38,10 +38,9 @@ module.exports = exports = function(webot){
             '重看本指令请回复help或问号',
             '更多指令请回复more',
             'PS: 点击下面的「查看全文」将跳转到我的github页'
-        ].join('\n')
-      };
+        ].join('\n')}
       // 返回值如果是list，则回复图文消息列表
-      return reply;
+      return reply.description;
     }
   });
 
@@ -51,7 +50,7 @@ module.exports = exports = function(webot){
       return rule.description;
     }).map(function(rule){
       //console.log(rule.name)
-      return '> ' + rule.description;
+      return '· ' + rule.description;
     }).join('\n').value();
 
     return ['我的主人还没教我太多东西,你可以考虑帮我加下.\n可用的指令:\n'+ reply,
@@ -103,7 +102,7 @@ module.exports = exports = function(webot){
   }, {
     name: 'time',
     description: '想知道几点吗? [Smart] 发送: time',
-    pattern: /^(几点了|time)\??$/i,
+    pattern: /^(几点了|time|时间)\??$/i,
     handler: function(info) {
       var d = new Date();
       var h = d.getHours();
@@ -361,9 +360,9 @@ module.exports = exports = function(webot){
     pattern: /^news\s*(\d*)$/,
     handler: function(info){
       var reply = [
-        {title: '微信机器人', description: '微信机器人测试帐号：webot', pic: 'https://raw.github.com/node-webot/webot-example/master/qrcode.jpg', url: 'https://github.com/node-webot/webot-example'},
-        {title: '豆瓣同城微信帐号', description: '豆瓣同城微信帐号二维码：douban-event', pic: 'http://i.imgur.com/ijE19.jpg', url: 'https://github.com/node-webot/weixin-robot'},
-        {title: '图文消息3', description: '图文消息描述3', pic: 'https://raw.github.com/node-webot/webot-example/master/qrcode.jpg', url: 'http://www.baidu.com'}
+        {title: '', description: '', pic: ''},
+        {title: '', description: '', pic: '', url: ''},
+        {title: '', description: '', pic: '', url: ''}
       ];
       // 发送 "news 1" 时只回复一条图文消息
       return Number(info.param[1]) == 1 ? reply[0] : reply;
